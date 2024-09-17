3351d6f08b6f54c7c0144b445665eabbd5adce87
nigguel <nigguel14@gmail.com>
2024-09-17 20:58:05 -0300

    Primeros pasos en FastAPI

---
+++ backend/FastAPI/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#    return{"message":"Hello World"}
+
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI!"
