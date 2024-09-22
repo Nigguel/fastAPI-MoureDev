d56ead411aa227939fc0a7c0ea5aeaa387269497
nigguel <nigguel14@gmail.com>
2024-09-21 23:28:39 -0300

    Adicionando operaciones get, post, put e delete a nuestra api

--- backend/FastAPI/users.py
+++ backend/FastAPI/users.py
@@ -45,7 +45,7 @@ async def usersjason():
     ]
 
 
-@app.get("/users")
+@app.get("/users/")
 async def users():
     return users_list
 
@@ -60,6 +60,50 @@ async def user(id: int):
 @app.get("/user/")
 async def user(id: int):
     return search_user(id)
+
+
+# operacion capaz de Insertar valores
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+        return user
+
+
+# Operacion capaz de actualizar valores
+@app.put("/user/")
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha encontrado el usuario"}
+    else:
+        return user
+
+
+# Operacion capaz dde borrar valores
+@app.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"error": "No se ha borrado el usuario"}
+    else:
+        return {"exito": "usuario borrado exitosamente"}
 
 
 def search_user(id: int):
